46f7ade036d3d241d5205b94814c399f165f7696
John Michael <john.parsons85210@gmail.com>
2017-08-18 15:41:21 -0700

    indent and make pretty

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,23 @@
-
 // BACK END LOGIC
 
 const ping = 3;
 const pong = 5;
-const pingPong = (ping*pong);
+const pingPong = (ping * pong);
 
 var pingPongCalc = function(number) {
   var inputArray = [];
   for (var i = 1; i <= number; i++) {
-
-    if (i % pingPong == 0 ) {
+    if (i % pingPong == 0) {
       inputArray.push("Ping-Pong!");
-    }
-    else if (i % pong == 0) {
+    } else if (i % pong == 0) {
       inputArray.push("Pong!");
-    }
-    else if (i % ping == 0) {
+    } else if (i % ping == 0) {
       inputArray.push("Ping!");
-    }
-    else {
+    } else {
       inputArray.push(i);
     }
   }
   return inputArray;
-
 };
 
 
@@ -31,12 +25,12 @@ var pingPongCalc = function(number) {
 
 $(document).ready(function() {
   $("form").submit(function(event) {
+    $("ul").empty();
     var number = ($("input#userInput").val());
     var outputArray = pingPongCalc(number);
-    if (number == 420){
+    if (number == 420) {
       alert("...");
-    }
-     $("ul").empty();
+    };
     for (var i = 0; i < outputArray.length; i++) {
       var listItem = document.createElement("li");
       listItem.innerHTML = outputArray[i];
