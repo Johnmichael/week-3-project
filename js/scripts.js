b90a6f4038d3e96385fffc638fc6a2605c6ba6f7
John Michael <john.parsons85210@gmail.com>
2017-08-18 15:34:55 -0700

    finalize javascript
    (non indented)

--- js/scripts.js
+++ js/scripts.js
@@ -3,61 +3,46 @@
 
 const ping = 3;
 const pong = 5;
-const pingPong = 15;
+const pingPong = (ping*pong);
 
-var inputNumbers = [];
-// const movies = ["The Wolf of Wall Street", "Zootopia", "Babysitting"];
-// for (let i = 0; i < movies.length; i++) {
-// console.log(movies[i]);
-// }
+var pingPongCalc = function(number) {
+  var inputArray = [];
+  for (var i = 1; i <= number; i++) {
 
+    if (i % pingPong == 0 ) {
+      inputArray.push("Ping-Pong!");
+    }
+    else if (i % pong == 0) {
+      inputArray.push("Pong!");
+    }
+    else if (i % ping == 0) {
+      inputArray.push("Ping!");
+    }
+    else {
+      inputArray.push(i);
+    }
+  }
+  return inputArray;
 
+};
 
-// var outputArray = map.inputArray();
-
-
-// var allowedUserInput = /[0-9]/
 
 // FRONT END LOGIC
 
 $(document).ready(function() {
   $("form").submit(function(event) {
-    var number = $("input#userInput").val();
-    function pingPongCheck() {
-      var bounce ="";
-      if (number % ping == 0) {
-        bounce = "ping"
-      };
-      if (number % pong== 0) {
-        bounce = "pong"
-      };
-      if (number % pingPong == 0) {
-        bounce = "ping-pong"
-      };
-      if (number % ping !=0 && number % pong != 0) {
-        bounce = number
-      };
-      return bounce;
-      inputNumbers.push(bounce);
-    };
-    $(".output").text(pingPongCheck);
+    var number = ($("input#userInput").val());
+    var outputArray = pingPongCalc(number);
+    if (number == 420){
+      alert("...");
+    }
+     $("ul").empty();
+    for (var i = 0; i < outputArray.length; i++) {
+      var listItem = document.createElement("li");
+      listItem.innerHTML = outputArray[i];
+      var numberList = document.getElementById("list");
+      numberList.appendChild(listItem);
+    }
     event.preventDefault();
-
-
-
-
   });
 });
-
-
-
-
-// Create for loop
-// var arrayMaker = function(num) {
-// 	var myArray = [];
-// 	for(var i = number; i <= num; i++){
-//   	// myArray.push(i);
-//     console.log(i);
-//   }
-//   return myArray;
-// };
